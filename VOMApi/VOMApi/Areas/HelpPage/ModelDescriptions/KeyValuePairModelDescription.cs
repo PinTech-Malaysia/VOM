0dd69e6d16533bd194239c6421e35a56bf081af6
Choy Wing Lun <c.w-lun96@hotmail.com>
2019-09-21 18:14:27 +0800

    Initiate vom Project

---
+++ VOMApi/VOMApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace VOMApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
